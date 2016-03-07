855a6f5c71f5b985e4aaadf55e8608d5e9c1e605
Omair <omairr.azam@gmail.com>
2016-03-07 05:41:33 +0500

    devise basic setup/Login setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  
+  devise_for :users
+
+  devise_scope :user do
+
+	  authenticated :user do
+	    root 'visitors#index', as: :authenticated_root
+	  end
+
+	  unauthenticated do
+	    root 'devise/sessions#new', as: :unauthenticated_root
+	  end
+
+	end
+  
 end
